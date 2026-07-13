37f5541b4021509156f4315024d4e21195e43def
Radhika Thakare <radhikathakare849@gmail.com>
2026-07-13 22:22:56 +0530

    Initial commit

---
+++ password_generator.py
@@ -0,0 +1,25 @@
+# Import required modules
+import random
+import string
+
+# Get password length from user
+length = int(input("Enter password length:"))
+
+# Check minimum password length
+if length < 8:
+    print("Warning: Password should be at least 8 characters long for better security.")
+
+# Define letters, digits and symbols
+else:
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+# Combine all characters
+    all_characters = letters + digits + symbols
+
+# Generate random password
+    password = "".join(random.choice(all_characters)for i in range(length))
+
+# Display password
+    print("Generated Password:",password)
